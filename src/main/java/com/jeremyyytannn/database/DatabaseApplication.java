cb7500ab80a93b8917e4627e70d4db3f517f6243
Jeremy <jeremyyytannn@gmail.com>
2024-04-22 15:29:16 +0800

    refactor: Initial code for database SQL

--- src/main/java/com/jeremyyytannn/database/DatabaseApplication.java
+++ src/main/java/com/jeremyyytannn/database/DatabaseApplication.java
@@ -1,31 +1,11 @@
 package com.jeremyyytannn.database;
 
-import javax.sql.DataSource;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import lombok.extern.java.Log;
 
 @SpringBootApplication
-@Log
-public class DatabaseApplication implements CommandLineRunner {
-  private final DataSource dataSource;
-
-  public DatabaseApplication(final DataSource dataSource) {
-    this.dataSource = dataSource;
-  }
-
+public class DatabaseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseApplication.class, args);
-	}
-
-  @Override
-	public void run(final String... args) {
-		log.info("Datasource: " + dataSource.toString());
-		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
-		restTemplate.execute("select 1");
 	}
 }
